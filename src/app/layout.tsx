2918092a6a6b8d9aa3d5bf99c4333b902726efb3
Martin Ouimet <mouimet@infinisoft.world>
2025-03-10 04:23:00 -0400

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Spencer Sharp',
+    default:
+      'Software engineer, founder, and knowledge lover.',
+  },
+  description:
+    'I’m Martin, since my early life, I discovered a deep passion to think, learn, understand and create. At the age of 12, I discovered software programming. That lit a fire inside of me, which is still burning after 27 years. That passion acted as a magnet across time and makes it possible to accomplish awesome acheivements.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
